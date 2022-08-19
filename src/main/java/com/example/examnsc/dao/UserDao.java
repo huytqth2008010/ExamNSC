cd98732d3a190dca0dfebf7d2b09070dcdf5fcc8
huytqth2008010 <70880121+huytqth2008010@users.noreply.github.com>
2022-08-19 09:46:37 +0700

    Initial commit

---
+++ src/main/java/com/example/examnsc/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.example.examnsc.dao;
+
+
+
+import com.example.examnsc.model.User;
+import com.example.examnsc.utils.ConnectionUtils;
+
+import java.sql.*;
+
+public class UserDao {
+    Connection connection = ConnectionUtils.getConnection();
+
+    public User getUser(String pName, String password) {
+        User user = null;
+        try {
+            Statement statement = connection.createStatement();
+            String sqlStatement = String.format("select * from users where username = '"+pName +"' and password = " +password);
+            ResultSet rs = statement.executeQuery(sqlStatement);
+            while (rs.next()){
+                String name = rs.getString("username");
+                String pass = rs.getString("password");
+                user = new User(name, pass);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+}
---
+++ src/main/java/com/example/examnsc/utils/ConnectionUtils.java
@@ -0,0 +1,39 @@
+package com.example.examnsc.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+
+    private static Connection connection;
+
+    public synchronized static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/ExamNSC";
+        String username = "root";
+        String password = "";
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(url, username, password);
+                System.out.println("Connect success !");
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
+        }else {
+            System.out.println("Use existing connection !");
+        }
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            connection = null;
+        }
+    }
+}
